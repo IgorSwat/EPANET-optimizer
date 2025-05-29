02d4403869eeecb2e396f302a49551246e949198
rpiwowar02 <115703243+rpiwowar02@users.noreply.github.com>
2025-05-29 18:29:03 +0200

    add benchmark functions
    add benchmark functions

--- elixir/lib/problem.ex
+++ elixir/lib/problem.ex
@@ -1,39 +1,35 @@
 defmodule Problem do
-  @type t :: %Problem{
-          name: String.t() | nil,
-          d: integer(),
-          fun: (Nx.Tensor.t() -> float()) | nil,
-          l: Nx.Tensor.t() | nil,
-          u: Nx.Tensor.t() | nil,
-          minimize: boolean(),
-        }
-
+  @derive {Nx.Container, containers: [:l, :u]}
+  @enforce_keys [:d]
   defstruct name: nil,
-            d: nil,
+            d: 10,
             fun: nil,
             l: nil,
             u: nil,
             minimize: true
 
-  @spec new(integer()) :: t()
-  def new(opts \\ %{}) do
-    d = Map.get(opts, :d, 3)
+  @type t :: %__MODULE__{
+          name: String.t() | nil,
+          d: integer(),
+          fun: (Nx.Tensor.t() -> float()) | nil,
+          l: Nx.Tensor.t() | nil,
+          u: Nx.Tensor.t() | nil,
+          minimize: boolean()
+        }
+
+  @spec new(map()) :: t()
+  def new(%{d: d} = opts) do
     fun = case Map.get(opts, :minimize, true) do
-      
       true -> Map.get(opts, :fun, nil)
       false -> fn tensor -> -Map.get(opts, :fun, nil).(tensor) end
     end
-    
+
     computed_fields = %{
       l: Nx.broadcast(-10, {d}),
       u: Nx.broadcast(10, {d}),
       fun: fun
-
     }
-    %__MODULE__{}
-    |> struct(Map.merge(computed_fields, opts))
-    |> struct(opts)
-      
+
+    struct(__MODULE__, Map.merge(computed_fields, opts))
   end
 end
-
---
+++ elixir/lib/zakharov.ex
@@ -0,0 +1,15 @@
+defmodule Zakharov do
+  import Nx.Defn
+  use SharedMacro
+
+  defn get_d, do: @d
+
+  defn evaluate_nx_matrix_defn(u) do
+    indices = Nx.iota({Nx.axis_size(u, 1)}) |> Nx.add(1) |> Nx.multiply(0.5)
+    sum_square_term = Nx.sum(u ** 2, axes: [1])
+    linear_term = Nx.sum(indices * u, axes: [1])
+    quadratic_term = linear_term ** 2
+    quartic_term = linear_term ** 4
+    sum_square_term + quadratic_term + quartic_term
+  end
+end
