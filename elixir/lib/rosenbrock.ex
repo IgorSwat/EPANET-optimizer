331b85fdf1d8359fd3c96913567f748f2bb10fbf
rpiwowar02 <115703243+rpiwowar02@users.noreply.github.com>
2025-05-29 18:27:50 +0200

    improved nx
    improved nx

--- elixir/lib/rosenbrock.ex
+++ elixir/lib/rosenbrock.ex
@@ -1,20 +1,15 @@
 defmodule Rosenbrock do
   import Nx.Defn
+  @a 1
+  @b 100
+  use SharedMacro
+  defn get_d, do: @d
+  defn evaluate_nx_matrix_defn(u) do
 
-  def evaluate_nx(u \\ Nx.tensor([1.0, 1.0, 1.0], type: {:f, 32}), a \\ 1.0, b \\ 100.0) do
-    d = Nx.size(u)
-    true_solution = Nx.tensor(List.duplicate(2.0, d))
-    evaluate_nx_defn(u, true_solution, a, b)
+    x1 = Nx.slice(u, [0, 0], [Nx.axis_size(u, 0), get_d() - 1])
+    x2 = Nx.slice(u, [0, 1], [Nx.axis_size(u, 0), get_d() - 1])
+
+    Nx.sum((@a - x1) ** 2 + @b * (x2 - x1 ** 2) ** 2, axes: [1])
   end
 
-  defn evaluate_nx_defn(u, true_solution, a, b) do
-    adjusted_u = Nx.subtract(u, true_solution)
-    size_u = Nx.size(adjusted_u)
-    
-    # Note: Nx.slice/4 expects start indices and slice lengths as lists.
-    x1 = Nx.slice(adjusted_u, [0], [size_u - 1])
-    x2 = Nx.slice(adjusted_u, [1], [size_u - 1])
-    
-    Nx.sum((a - x1) ** 2 + b * (x2 - x1 ** 2) ** 2)
-  end
 end
